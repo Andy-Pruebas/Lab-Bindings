8fef10d7491aea3166d740dc2643a07e196b44dd
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-06-02 12:49:39 -0500

    Primer Commit by Andy

---
+++ Binding/Binding/Binding/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+
+namespace Binding
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewToViewDemo.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ViewToViewDemo());
+            };
+            BindingModeDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new BindingModeDemo());
+            };
+            ListViewDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new ListViewDemo());
+            };
+            PickerDemo.Clicked += async (sender, e) => {
+                await Navigation.PushAsync(new PickerDemo());
+            };
+        }
+    }
+}
